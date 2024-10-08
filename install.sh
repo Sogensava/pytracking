d0afbfb413c59ebd98422603766f88d8ca5abbcb
Sogensava <aliberksrn@gmail.com>
2024-10-08 19:24:47 +0300

    asdas

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
 conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing matplotlib ******************"
 conda install -y matplotlib
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing pandas ******************"
 conda install -y pandas
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing tqdm ******************"
 conda install -y tqdm
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing opencv ******************"
 pip install opencv-python
 
@@ -30,65 +30,65 @@ echo ""
 echo "****************** Installing tensorboard ******************"
 pip install tb-nightly
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing visdom ******************"
 pip install visdom
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing scikit-image ******************"
 pip install scikit-image
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing tikzplotlib ******************"
 pip install tikzplotlib
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing gdown ******************"
 pip install gdown
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing cython ******************"
 conda install -y cython
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing coco toolkit ******************"
 pip install pycocotools
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing LVIS toolkit ******************"
 pip install lvis
 
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "******** Installing spatial-correlation-sampler. Note: This is required only for KYS tracker **********"
 pip install spatial-correlation-sampler
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing jpeg4py python wrapper ******************"
 pip install jpeg4py 
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Installing ninja-build to compile PreROIPooling ******************"
 echo "************************* Need sudo privilege ******************"
 sudo apt-get install ninja-build
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** Downloading networks ******************"
 mkdir pytracking/networks
 
-echo ""
-echo ""
+# echo ""
+# echo ""
 echo "****************** DiMP50 Network ******************"
 gdown https://drive.google.com/uc\?id\=1qgachgqks2UGjKx-GdO1qylBDdB1f9KN -O pytracking/networks/dimp50.pth
 gdown https://drive.google.com/uc\?id\=1MAjrRJDCbL0DSjUKFyDkUuYS1-cYBNjk -O pytracking/networks/dimp18.pth
@@ -103,16 +103,16 @@ gdown https://drive.google.com/uc\?id\=1MAjrRJDCbL0DSjUKFyDkUuYS1-cYBNjk -O pytr
 # echo "****************** ECO Network ******************"
 # gdown https://drive.google.com/uc\?id\=1aWC4waLv_te-BULoy0k-n_zS-ONms21S -O pytracking/networks/resnet18_vggmconv1.pth
 
-echo ""
-echo ""
-echo "****************** Setting up environment ******************"
-python -c "from pytracking.evaluation.environment import create_default_local_file; create_default_local_file()"
-python -c "from ltr.admin.environment import create_default_local_file; create_default_local_file()"
+# echo ""
+# echo ""
+# echo "****************** Setting up environment ******************"
+# python -c "from pytracking.evaluation.environment import create_default_local_file; create_default_local_file()"
+# python -c "from ltr.admin.environment import create_default_local_file; create_default_local_file()"
 
 
-echo ""
-echo ""
-echo "****************** Installing jpeg4py ******************"
+# echo ""
+# echo ""
+# echo "****************** Installing jpeg4py ******************"
 # while true; do
 #     read -p "Install jpeg4py for reading images? This step required sudo privilege. Installing jpeg4py is optional, however recommended. [y,n]  " install_flag
 #     case $install_flag in
