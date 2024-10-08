bba161f3000e21b1fd8f427ecfeece63a0563b48
Sogensava <aliberksrn@gmail.com>
2024-10-08 19:33:33 +0300

    a

--- install.sh
+++ install.sh
@@ -91,31 +91,4 @@ mkdir pytracking/networks
 # echo ""
 echo "****************** DiMP50 Network ******************"
 gdown https://drive.google.com/uc\?id\=1qgachgqks2UGjKx-GdO1qylBDdB1f9KN -O pytracking/networks/dimp50.pth
-gdown https://drive.google.com/uc\?id\=1MAjrRJDCbL0DSjUKFyDkUuYS1-cYBNjk -O pytracking/networks/dimp18.pth
 
-# echo ""
-# echo ""
-# echo "****************** ATOM Network ******************"
-# gdown https://drive.google.com/uc\?id\=1VNyr-Ds0khjM0zaq6lU-xfY74-iWxBvU -O pytracking/networks/atom_default.pth
-
-# echo ""
-# echo ""
-# echo "****************** ECO Network ******************"
-# gdown https://drive.google.com/uc\?id\=1aWC4waLv_te-BULoy0k-n_zS-ONms21S -O pytracking/networks/resnet18_vggmconv1.pth
-
-# echo ""
-# echo ""
-# echo "****************** Setting up environment ******************"
-# python -c "from pytracking.evaluation.environment import create_default_local_file; create_default_local_file()"
-# python -c "from ltr.admin.environment import create_default_local_file; create_default_local_file()"
-
-
-
-# echo ""
-# echo ""
-echo "****************** Installation complete! ******************"
-
-# echo ""
-# echo ""
-# echo "****************** More networks can be downloaded from the google drive folder https://drive.google.com/drive/folders/1WVhJqvdu-_JG1U-V0IqfxTUa1SBPnL0O ******************"
-# echo "****************** Or, visit the model zoo at https://github.com/visionml/pytracking/blob/master/MODEL_ZOO.md ******************"
